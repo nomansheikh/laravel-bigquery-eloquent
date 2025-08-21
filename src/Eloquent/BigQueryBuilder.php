4cb055fd7c25d6b9acb7d7a7307e7eb67a05fe77
Noman Sheikh <nomansheak3@gmail.com>
2025-08-21 13:37:19 +0600

    Add `first` method to BigQueryBuilder for retrieving single rows
    Signed-off-by: Noman Sheikh <nomansheak3@gmail.com>

--- src/Eloquent/BigQueryBuilder.php
+++ src/Eloquent/BigQueryBuilder.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Builder;
 
 class BigQueryBuilder extends Builder
 {
-    //
+    public function first($columns = ['*']): self
+    {
+        $this->limit(1)->get($columns);
+
+        return $this;
+    }
 }
